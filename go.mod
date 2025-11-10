45ce9b299b2445ae2ad230b1a1a1bfd602eaf9cb
Damian <dydekpyrcz@gmail.com>
2025-11-10 21:04:31 +0100

    TODO: salary and contract selector, tests

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/pfczx/jobscraper
 
-go 1.24.8
+go 1.25.3
 
 require (
 	github.com/PuerkitoBio/goquery v1.10.3 // indirect
@@ -16,6 +16,7 @@ require (
 	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+	github.com/pyrczuu/urlScraper v1.0.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.46.0 // indirect
--- iternal/scraper/scrapers/nofluff_scraper.go
+++ iternal/scraper/scrapers/nofluff_scraper.go
@@ -2,56 +2,57 @@ package scrapers
 
 import (
 	"context"
-	"github.com/gocolly/colly"
-	"github.com/pfczx/jobscraper/iternal/scraper"
 	"log"
 	"strings"
 	"time"
+
+	"github.com/gocolly/colly"
+	"github.com/pfczx/jobscraper/iternal/scraper"
 )
 
 const (
-	titleSelector         = "h1[data-scroll-id='job-title']"
-	companySelector       = "h2[data-scroll-id='employer-name']"
-	locationSelector      = "div[data-test='offer-badge-title']"
-	descriptionSelector   = `ul[data-test="text-about-project"]`                                                         //concat in code
-	skillsSelector        = `span[data-test="item-technologies-expected"], span[data-test="item-technologies-optional"]` //concat in code
-	salarySectionSelector = `div[data-test="section-salaryPerContractType"]`
+	titleSelector         = "h1.posting-details-description"
+	companySelector       = "a#postingCompanyUrl"
+	locationSelector      = "span[data-cy='location_pin']"
+	descriptionSelector   = `section#posting-description`                            //concat in code
+	skillsSelector        = `div#posting-requirements, section#posting-nice-to-have` //concat in code
+	salarySectionSelector = `div.salary`
 	salaryAmountSelector  = `div[data-test="text-earningAmount"]`
 	contractTypeSelector  = `span[data-test="text-contractTypeName"]`
 )
 
-type PracujScraper struct {
+type NoFluffScraper struct {
 	timeoutBetweenScraps time.Duration
 	collector            *colly.Collector
 	urls                 []string
 }
 
 // controls
-func NewPracujScraper(urls []string) *PracujScraper {
+func NewNoFLuffScraper(urls []string) *NoFluffScraper {
 	c := colly.NewCollector(
-		colly.AllowedDomains("www.pracuj.pl", "pracuj.pl"),
+		colly.AllowedDomains("www.nofluffjobs.com", "nofluffjobs.com"),
 		//colly.Async(true),
 	)
 
-// #nosec G104 - false positive i guess
+	// #nosec G104 - false positive i guess
 	c.Limit(&colly.LimitRule{
-		DomainGlob:  "*pracuj.pl*",
+		DomainGlob:  "*nofluffjobs.com*",
 		Parallelism: 2,
 		RandomDelay: 2 * time.Second,
 	})
 
-	return &PracujScraper{
+	return &NoFluffScraper{
 		timeoutBetweenScraps: 10 * time.Second,
 		collector:            c,
 		urls:                 urls,
 	}
 }
 
-func (*PracujScraper) Source() string {
-	return "pracuj.pl"
+func (*NoFluffScraper) Source() string {
+	return "nofluffjobs.com"
 }
 
-func (p *PracujScraper) Scrape(ctx context.Context, q chan<- scraper.JobOffer) error {
+func (p *NoFluffScraper) Scrape(ctx context.Context, q chan<- scraper.JobOffer) error {
 	p.collector.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 
 	p.collector.OnHTML("html", func(e *colly.HTMLElement) {
